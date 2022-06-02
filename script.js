4d3854bd5cf3789dc22ab1c9fd718fac4910b4b8
Izabella Golovina <ia.golovina00@gmail.com>
2022-06-02 19:03:50 +0300

    Add functionality of delete button Correct division function Add rounding

--- script.js
+++ script.js
@@ -7,9 +7,14 @@ const operatorButtons = document.querySelectorAll('.operation-button');
 const equalButton = document.querySelector('#equal');
 const display = document.querySelector('.display');
 const clearButton = document.querySelector('#clear');
+const deleteButton = document.querySelector('#delete');
 
 clearButton.addEventListener('click', () => {
     clear();
+});
+
+deleteButton.addEventListener('click', () => {
+    deleteDigit();
 });
 
 digitButtons.forEach((button) => 
@@ -45,6 +50,16 @@ function clear() {
     display.textContent = '';
 }
 
+function deleteDigit() {
+    if (operator == '') {
+        firstArg = firstArg.slice(0, -1);
+        displayNum(firstArg);
+    } else {
+        secondArg = secondArg.slice(0, -1);
+        displayNum(secondArg);
+    }
+}
+
 function calculate(firstArg, secondArg, operator) {
     let a = parseInt(firstArg);
     let b = parseInt(secondArg);
@@ -56,10 +71,15 @@ function calculate(firstArg, secondArg, operator) {
             displayNum(a - b);
             break;
         case '%':
-            displayNum(a % b);
+            let division = Math.floor(a / b * 1000) / 1000;
+            displayNum(division);
             break;
         case '*':
             displayNum(a * b);
             break;
     }
 }
+
+//add functionality of delete button
+//correct division function
+//add rounding
